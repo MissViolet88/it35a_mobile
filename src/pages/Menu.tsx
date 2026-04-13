6120d5555948146a8e664982671185417e7797dc
MissViolet88 <ms.violetsp08@gmail.com>
2026-04-13 12:25:20 +0800

    Update the menu file to add side bar menu

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,60 +1,66 @@
-import { IonButton, IonButtons, IonIcon, IonContent, IonHeader, IonItem, IonMenu, IonMenuButton, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar, useIonRouter } from "@ionic/react";
-import { Redirect, Route } from "react-router";
-import { homeOutline, logOutOutline } from "ionicons/icons";
+import {IonPage,IonHeader,IonToolbar,IonTitle,IonContent,IonSplitPane,IonMenu,IonRouterOutlet,IonMenuToggle,IonIcon,IonItem,IonButton} from "@ionic/react";
+
+import { Redirect, Route } from "react-router-dom";
 import Home from "./Home";
+import About from "./About";
+import { homeOutline, logOutOutline } from "ionicons/icons";
 
 const Menu: React.FC = () => {
 
-    const path = [
-        { name: 'Home', url: '/app/home', icon: homeOutline }
-    ]
+  const paths = [
+    { name: 'Home', url: '/app/home', icon: homeOutline },
+    { name: 'About', url: '/app/about', icon: homeOutline }
+  ];
 
+  return (
+    <IonPage>
+      <IonSplitPane contentId="main">
 
+        {/* SIDE MENU */}
+        <IonMenu contentId="main">
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>Menu</IonTitle>
+            </IonToolbar>
+          </IonHeader>
 
-    return (
-        <IonPage>
-            <IonSplitPane contentId="main">
-                <IonMenu contentId="main">
+          <IonContent>
+            {paths.map((item, index) => (
+              <IonMenuToggle key={index}>
+                <IonItem routerLink={item.url} routerDirection="forward">
+                  <IonIcon icon={item.icon} slot="start" />
+                  {item.name}
+                </IonItem>
+              </IonMenuToggle>
+            ))}
 
-                    <IonHeader>
-                        <IonToolbar>
-                            <IonTitle>
-                                Menu
-                            </IonTitle>
-                        </IonToolbar>
-                    </IonHeader>
+            <IonButton routerLink="/app/home" routerDirection="forward" expand="full">
+              <IonIcon icon={homeOutline} slot="start" />
+              Home
+            </IonButton>
 
-                    <IonContent>
+            <IonButton routerLink="/app/about" routerDirection="forward" expand="full">
+            About
+            </IonButton>
 
-                        {path.map((item, index) => (
-                            <IonMenuToggle key={index}>
-                                <IonItem routerLink={item.url} routerDirection="forward">
-                                    <IonIcon icon={item.icon} slot="start">
-                                        {item.name}
-                                    </IonIcon>
-                                </IonItem>
-                            </IonMenuToggle>
-                        ))}
-                        <IonButton routerLink="/" routerDirection="back" expand="full">
-                            <IonIcon icon={logOutOutline} slot="start" ></IonIcon>
-                            Logout
-                        </IonButton>
+            <IonButton routerLink="/" routerDirection="back" expand="full">
+              <IonIcon icon={logOutOutline} slot="start" />
+              Logout
+            </IonButton>
+          </IonContent>
+        </IonMenu>
 
-                    </IonContent>
-                </IonMenu>
+        {/* MAIN CONTENT */}
+        <IonRouterOutlet id="main">
+          <Route exact path="/app/home" component={Home} />
+          <Route exact path="/app">
+            <Redirect to="/app/home" />
+          </Route>
+        </IonRouterOutlet>
 
-                <IonRouterOutlet id="main">
-                    <Route exact path="/app/Home" component={Home} />
-                    <Route exact path="/app">
-                        <Redirect to="/app/Home" />
-                    </Route>
-
-                </IonRouterOutlet>
-
-            </IonSplitPane>
-        </IonPage>
-
-    );
-
+      </IonSplitPane>
+    </IonPage>
+  );
 };
+
 export default Menu;
