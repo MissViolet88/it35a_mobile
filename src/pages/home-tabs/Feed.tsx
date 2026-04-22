cdfbced27d8fdeab4a83e63efba7c8851c48f96a
MissViolet88 <ms.violetsp08@gmail.com>
2026-04-22 19:58:48 +0800

    added movies

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,6 +1,15 @@
 import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from "@ionic/react";
 
 const Feed: React.FC = () => {
+
+    const Movies = [
+        { name: "Parasite" },
+        { name: "Train to Busan" },
+        { name: "Oldboy" },
+        { name: "The Handmaiden" },
+        { name: "Burning" },
+    ]
+
     return (
         <IonPage>
             <IonHeader>
