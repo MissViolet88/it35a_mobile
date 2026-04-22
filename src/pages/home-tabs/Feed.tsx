4674c5c6f81d8a083c8169773ea2e46759c120cc
MissViolet88 <ms.violetsp08@gmail.com>
2026-04-22 20:01:46 +0800

    added card in the feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,4 +1,4 @@
-import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from "@ionic/react";
+import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonList } from "@ionic/react";
 
 const Feed: React.FC = () => {
 
@@ -20,7 +20,23 @@ const Feed: React.FC = () => {
                     <IonTitle>Feed</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent fullscreen></IonContent>
+
+            <IonContent>
+                <IonList inset={true}>
+                    {Movies.map((item, index) => (
+                        <IonCard>
+                            <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                            <IonCardHeader>
+                                <IonCardTitle>{item.name}</IonCardTitle>
+                                <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                            </IonCardHeader>
+
+                            <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+                        </IonCard>
+                    ))}
+
+                </IonList>
+            </IonContent>
         </IonPage>
     );
 }
