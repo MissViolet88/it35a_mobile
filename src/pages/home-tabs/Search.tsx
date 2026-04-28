2cee7070a5bd6afb4d0ea05df1a89b4063b5320e
MissViolet88 <ms.violetsp08@gmail.com>
2026-04-28 21:15:43 +0800

    added search functionality that can search movies

--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,18 +1,78 @@
-import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonList, IonItem, IonLabel, IonCard, IonCardTitle, IonButton, IonIcon, IonGrid, IonCardContent, IonCardSubtitle, IonCardHeader, IonCol, IonRow, IonSearchbar, IonToolbar } from "@ionic/react";
+import { chatboxEllipsesOutline, shareOutline, thumbsUp, thumbsUpOutline } from "ionicons/icons";
+
 
 const Search: React.FC = () => {
+
+    const Movies = [
+        { name: "Parasite" },
+        { name: "Train to Busan" },
+        { name: "Oldboy" },
+        { name: "The Handmaiden" },
+        { name: "Burning" },
+    ]
+
+    const [searchText, setSearchText] = useState('');
+    const [filtered, setFilteredGames] = useState(Movies);
+
+    useEffect(() => {
+        const debounce = setTimeout(() => {
+            const filteredGames = Movies.filter(Movies =>
+                Movies.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredGames(filteredGames);
+        }, 400);
+
+        return () => clearTimeout(debounce);
+    }, [searchText]);
+
+
+
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
                     <IonButtons>
                         <IonMenuButton></IonMenuButton>
+                        <IonTitle>
+                            Search
+                        </IonTitle>
                     </IonButtons>
-                    <IonTitle>Search</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent fullscreen></IonContent>
+
+            <IonContent className='ion-padding'>
+
+                <IonSearchbar
+                    placeholder="Search for movies..."
+                    value={searchText}
+                    debounce={0}
+                    onIonInput={e => setSearchText(e.detail.value!)}
+                />
+
+                <IonList>
+                    {filtered.map((item, index) => (
+                        <IonCard>
+                            <IonGrid>
+                                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                                <IonCardHeader>
+                                    <IonCardTitle>{item.name}</IonCardTitle>
+                                    <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                                </IonCardHeader>
+
+                                <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+                                <IonButton fill="outline">Like</IonButton>
+                                <IonButton fill="outline">Comment</IonButton>
+                                <IonButton fill="outline">Share</IonButton>
+                            </IonGrid>
+                        </IonCard>
+                    ))}
+                </IonList>
+            </IonContent>
         </IonPage>
+
     );
-}
+};
+
 export default Search;
